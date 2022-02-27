0643d6ed7e9c4771570ea2eac311ac85a4516dcd
gohoon-k <hoonkun@unstabler.pl>
2022-02-27 19:36:27 +0900

    [feature] /pixel reset 커맨드의 기능 추가

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/ResetExecutor.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/ResetExecutor.kt
@@ -1,11 +1,23 @@
 package kiwi.hoonkun.plugins.pixel.commands
 
+import kiwi.hoonkun.plugins.pixel.Entry
 import org.bukkit.command.CommandSender
 
 class ResetExecutor: Executor() {
 
     override fun exec(sender: CommandSender?, args: List<String>): CommandExecuteResult {
-        TODO("implement this function")
+        return if (args[0].toIntOrNull() != null)
+            spawn(listOf("git", "reset", "--hard", "HEAD~${args[0]}"), Entry.versionedFolder!!)
+                .handle(
+                    "successfully reset HEAD to ${args[0]} behind commit",
+                    "failed to reset"
+                )
+        else
+            spawn(listOf("git", "reset", "--hard", args[0]), Entry.versionedFolder!!)
+                .handle(
+                    "successfully reset HEAD to commit '${args[0]}'",
+                    "failed to reset"
+                )
     }
 
     override fun autoComplete(args: List<String>): MutableList<String> {
