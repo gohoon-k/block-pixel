4f4c87a69d254fa1a44e5f777d26a001b587638e
gohoon-k <hoonkun@unstabler.pl>
2022-02-27 22:09:54 +0900

    [test] 커맨드 테스트 코드 작성

--- src/test/kotlin/ExecutorTest.kt
+++ src/test/kotlin/ExecutorTest.kt
@@ -1,26 +1,126 @@
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import kiwi.hoonkun.plugins.pixel.Entry
-import kiwi.hoonkun.plugins.pixel.commands.CommitExecutor
-import kiwi.hoonkun.plugins.pixel.commands.InitializeExecutor
+import kiwi.hoonkun.plugins.pixel.commands.*
 import java.io.File
 
-class ExecutorTest: StringSpec() {
+class Environment {
 
-    init {
+    companion object {
 
-        val dataFolder = "../_main/plugins/pixel"
-        Entry.dataFolder = File(dataFolder)
-        Entry.versionedFolder = File("$dataFolder/versioned")
-        Entry.clientFolder = File("$dataFolder/../..")
-        Entry.levelName = "world"
-        Entry.logFolder = File("$dataFolder/logs")
-
-        "git init + commit" {
-            InitializeExecutor().exec(null, listOf())
-
-            CommitExecutor().exec(null, listOf("initial commit."))
+        fun setup() {
+            val dataFolder = "../_main/plugins/pixel"
+            Entry.dataFolder = File(dataFolder)
+            Entry.versionedFolder = File("$dataFolder/versioned")
+            Entry.clientFolder = File("$dataFolder/../..")
+            Entry.levelName = "world"
+            Entry.logFolder = File("$dataFolder/logs")
         }
 
+    }
+
+}
+
+class PixelInitializeTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel init" {
+            val result = InitializeExecutor().exec(null, listOf())
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelCommitTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel commit" {
+            val result = CommitExecutor().exec(null, listOf("test commit in ${System.currentTimeMillis()}"))
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelBranchTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel branch" {
+            val result = BranchExecutor().exec(null, listOf("struct/library"))
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelCheckoutTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel checkout" {
+            val result = CheckoutExecutor().exec(null, listOf("master"))
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelDiscardTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel discard" {
+            val result = DiscardExecutor().exec(null, listOf())
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelResetTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel reset" {
+            val result = ResetExecutor().exec(null, listOf("2"))
+            println(result.message)
+            result.success shouldBe true
+        }
+    }
+
+}
+
+class PixelListTest: StringSpec() {
+
+    init {
+        Environment.setup()
+
+        "/pixel list commits" {
+            val result = ListExecutor().exec(null, listOf("commits"))
+            println(result.message)
+            result.success shouldBe true
+        }
+
+        "/pixel list branches" {
+            val result = ListExecutor().exec(null, listOf("branches"))
+            println(result.message)
+            result.success shouldBe true
+        }
     }
 
 }
