d3cc6fec07506d6e089412d58ea46a7f226ee421
gohoon-k <hoonkun@unstabler.pl>
2022-02-27 19:40:55 +0900

    [fix] cc28e7d0 커밋의 사항이 반영되지 않은 문제 수정

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/DiscardExecutor.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/DiscardExecutor.kt
@@ -5,9 +5,9 @@ import org.bukkit.command.CommandSender
 
 class DiscardExecutor: Executor() {
 
-    override fun exec(sender: CommandSender?, args: List<String>): Boolean {
+    override fun exec(sender: CommandSender?, args: List<String>): CommandExecuteResult {
         WriteWorker.versioned2client(listOf("overworld", "nether", "the_end"))
-        return true
+        return CommandExecuteResult(true, "successfully discard uncommitted changes")
     }
 
     override fun autoComplete(args: List<String>): MutableList<String> {
