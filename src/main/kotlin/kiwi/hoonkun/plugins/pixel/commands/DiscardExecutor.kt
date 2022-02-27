6ea4e9a1e5d837ed33414586bd40f4ef1f7a7ec9
gohoon-k <hoonkun@unstabler.pl>
2022-02-27 19:02:58 +0900

    [feature] /pixel discard 커맨드 기능 추가

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/DiscardExecutor.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/DiscardExecutor.kt
@@ -1,10 +1,12 @@
 package kiwi.hoonkun.plugins.pixel.commands
 
+import kiwi.hoonkun.plugins.pixel.worker.WriteWorker
 import org.bukkit.command.CommandSender
 
 class DiscardExecutor: Executor() {
 
     override fun exec(sender: CommandSender?, args: List<String>): Boolean {
+        WriteWorker.versioned2client(listOf("overworld", "nether", "the_end"))
         return true
     }
 
