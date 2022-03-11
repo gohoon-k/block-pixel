e87d5af3382cf7a0cb146afdd6e71f8e18972a79
gohoon-k <hoonkun@unstabler.pl>
2022-03-11 18:03:19 +0900

    [fix] 다수의 글자 크기가 잘못 입력되어있던 문제 수정

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/utils/TextWidthUtils.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/utils/TextWidthUtils.kt
@@ -7,12 +7,12 @@ class TextWidthUtils {
     companion object {
 
         private val widths = mapOf(
-            'a' to 4, 'b' to 4, 'c' to 4, 'd' to 4, 'e' to 4, 'f' to 3, 'g' to 4, 'h' to 4, 'i' to 1, 'j' to 4, 'k' to 4, 'l' to 2, 'm' to 5,
-            'n' to 4, 'o' to 4, 'p' to 4, 'q' to 4, 'r' to 4, 's' to 4, 't' to 3, 'u' to 4, 'v' to 5, 'w' to 5, 'x' to 5, 'y' to 4, 'z' to 5,
-            'A' to 4, 'B' to 4, 'C' to 4, 'D' to 4, 'E' to 4, 'F' to 4, 'G' to 4, 'H' to 4, 'I' to 3, 'J' to 4, 'K' to 4, 'L' to 4, 'M' to 5,
-            'N' to 5, 'O' to 4, 'P' to 4, 'Q' to 4, 'R' to 4, 'S' to 4, 'T' to 5, 'U' to 4, 'V' to 5, 'W' to 5, 'X' to 5, 'Y' to 5, 'Z' to 5,
+            'a' to 5, 'b' to 5, 'c' to 5, 'd' to 5, 'e' to 5, 'f' to 4, 'g' to 5, 'h' to 5, 'i' to 1, 'j' to 5, 'k' to 4, 'l' to 2, 'm' to 5,
+            'n' to 5, 'o' to 5, 'p' to 5, 'q' to 5, 'r' to 5, 's' to 5, 't' to 3, 'u' to 4, 'v' to 5, 'w' to 5, 'x' to 5, 'y' to 5, 'z' to 5,
+            'A' to 5, 'B' to 5, 'C' to 5, 'D' to 5, 'E' to 5, 'F' to 5, 'G' to 5, 'H' to 5, 'I' to 3, 'J' to 5, 'K' to 5, 'L' to 5, 'M' to 5,
+            'N' to 5, 'O' to 5, 'P' to 5, 'Q' to 5, 'R' to 5, 'S' to 5, 'T' to 5, 'U' to 5, 'V' to 5, 'W' to 5, 'X' to 5, 'Y' to 5, 'Z' to 5,
             '1' to 5, '2' to 5, '3' to 5, '4' to 5, '5' to 5, '6' to 5, '7' to 5, '8' to 5, '9' to 5, '0' to 5, '"' to 3, '\'' to 1, '/' to 5,
-            '\\' to 5, '-' to 4, '_' to 4, ' ' to 2, '(' to 3, ')' to 3, '[' to 2, ']' to 2, '<' to 4, '>' to 4, '.' to 1, ':' to 1, ';' to 1
+            '\\' to 5, '-' to 4, '_' to 4, ' ' to 3, '(' to 3, ')' to 3, '[' to 2, ']' to 2, '<' to 4, '>' to 4, '.' to 1, ':' to 1, ';' to 1, '|' to 1
         )
 
         private const val letterSpacing = 1
