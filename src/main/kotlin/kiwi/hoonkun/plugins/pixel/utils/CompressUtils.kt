008bf4cecef04809a74c55eb24372532d01b774b
gohoon-k <hoonkun@unstabler.pl>
2022-03-14 11:27:42 +0900

    [feature] GZip 압축/해제 구현 추가

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/utils/CompressUtils.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/utils/CompressUtils.kt
@@ -1,7 +1,12 @@
 package kiwi.hoonkun.plugins.pixel.utils
 
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.util.zip.Deflater
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 import java.util.zip.Inflater
 
 class CompressUtils {
@@ -41,6 +46,40 @@ class CompressUtils {
                 }
 
                 return compressStream.toByteArray()
+            }
+
+        }
+
+    }
+
+    class GZip {
+
+        companion object {
+
+            fun decompress(input: ByteArray): ByteArray {
+                val inputStream = GZIPInputStream(BufferedInputStream(ByteArrayInputStream(input)))
+                val outputStream = ByteArrayOutputStream()
+
+                val buffer = ByteArray(1024)
+                do {
+                    val size = inputStream.read(buffer)
+                    if (size > 0) outputStream.write(buffer, 0, size)
+                } while (size > 0)
+
+                outputStream.flush()
+                outputStream.close()
+
+                return outputStream.toByteArray()
+            }
+
+            fun compress(input: ByteArray): ByteArray {
+                val outputStream = ByteArrayOutputStream()
+                val gzip = GZIPOutputStream(BufferedOutputStream(outputStream))
+                gzip.write(input)
+                gzip.finish()
+                gzip.close()
+
+                return outputStream.toByteArray()
             }
 
         }
