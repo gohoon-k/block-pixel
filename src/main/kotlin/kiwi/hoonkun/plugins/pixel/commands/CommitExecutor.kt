dbb64fafe88c8d17ce8665aa870c07c6fb5c4cc8
gohoon-k <hoonkun@unstabler.pl>
2022-03-16 16:22:59 +0900

    [feature] amend 커밋 기능 추가 - amend 커밋이 가능할 경우 자동완성에 -amend 항목 표시 - amend 커밋이 가능하고 -amend 옵션이 설정되었을 경우 커밋 메시지 인수의 자동완성으로 이전 커밋의 메시지를 출력 - all 을 world 인수에 전달했을 경우, amend 커밋은 모든 월드의 마지막 커밋 메시지가 동일할 때만 가능하다.

--- src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/CommitExecutor.kt
+++ src/main/kotlin/kiwi/hoonkun/plugins/pixel/commands/CommitExecutor.kt
@@ -12,13 +12,37 @@ class CommitExecutor(parent: Entry): Executor(parent) {
     companion object {
 
         const val MESSAGE_NO_REPOSITORY = "repository with given world is not initialized!"
+        private const val MESSAGE_CANNOT_AMEND = "cannot to amend commit"
+        private const val MESSAGE_CANNOT_AMEND_ALL = "$MESSAGE_CANNOT_AMEND with 'all' world alias"
+        private const val MESSAGE_CANNOT_AMEND_ALL_DIFF = "$MESSAGE_CANNOT_AMEND_ALL, because each worlds' latest commits have difference messages."
+        private const val MESSAGE_CANNOT_AMEND_ALL_NO_COMMIT = "$MESSAGE_CANNOT_AMEND_ALL, because some repositories have no base commit to amend."
+        private const val MESSAGE_CANNOT_AMEND_NO_COMMIT = "$MESSAGE_CANNOT_AMEND, because specified world repository has no base commit to amend."
 
         const val SUGGEST_NO_REPOSITORY = "please run '/pixel init' first."
         const val SUGGEST_DETACHED_HEAD = "please create branch using '/pixel branch' before commit."
+        private const val SUGGEST_CANNOT_AMEND_ALL_DIFF = "try amend commit individually."
+        private const val SUGGEST_CANNOT_AMEND_ALL_NO_COMMIT = "try amend commit individually or make base commit of amend commit."
+        private const val SUGGEST_CANNOT_AMEND_NO_COMMIT = "try make a normal(base) commit first."
+
+        val RESULT_NO_COMMIT_EXECUTED =
+            CommandExecuteResult(true, "there are no repositories exists. nothing happened.")
+
+        val RESULT_CANNOT_AMEND_ALL_DIFF =
+            CommandExecuteResult(false, "$MESSAGE_CANNOT_AMEND_ALL_DIFF\n$y$SUGGEST_CANNOT_AMEND_ALL_DIFF")
+
+        val RESULT_CANNOT_AMEND_ALL_NO_COMMIT =
+            CommandExecuteResult(false, "$MESSAGE_CANNOT_AMEND_ALL_NO_COMMIT\n$y$SUGGEST_CANNOT_AMEND_ALL_NO_COMMIT")
+
+        val RESULT_CANNOT_AMEND_NO_COMMIT =
+            CommandExecuteResult(false, "$MESSAGE_CANNOT_AMEND_NO_COMMIT\n$y$SUGGEST_CANNOT_AMEND_NO_COMMIT")
+
+        const val AMEND_ARG = "-amend"
+
+        val SECOND_ARGS_LIST = mutableListOf(AMEND_ARG)
 
     }
 
-    override val usage: String = "commit < world > < commit_message >"
+    override val usage: String = "commit < world > [ \"$AMEND_ARG\" ] < commit_message >"
     override val description: String = "creates new commit to given world's repository, in current branch."
 
     override suspend fun exec(sender: CommandSender?, args: List<String>): CommandExecuteResult {
@@ -26,7 +50,10 @@ class CommitExecutor(parent: Entry): Executor(parent) {
             return createNotEnoughArgumentsResult(listOf(2), args.size)
 
         val world = args[0]
-        val message = args.slice(1 until args.size).joinToString(" ")
+        var message = args.toMutableList()
+            .apply { if (args[1] == AMEND_ARG) removeAt(1) }
+            .let { it.slice(1 until it.size) }
+            .joinToString(" ")
 
         if (!isValidWorld(world) && world != "all")
             return createUnknownWorldResult(world)
@@ -52,7 +79,25 @@ class CommitExecutor(parent: Entry): Executor(parent) {
         }
 
         if (targets.size == 0)
-            return CommandExecuteResult(true, "there are no repositories exists. nothing happened.")
+            return RESULT_NO_COMMIT_EXECUTED
+
+        val amend = args[1] == AMEND_ARG
+
+        if (targets.size != 1 && amend) {
+            val messages = targets.map { parent.lastCommit[it]?.fullMessage }.toSet()
+            if (messages.contains(null))
+                return RESULT_CANNOT_AMEND_ALL_NO_COMMIT
+            if (messages.size != 1)
+                return RESULT_CANNOT_AMEND_ALL_DIFF
+            if (message.isEmpty())
+                message = messages.toList()[0]!!
+        }
+
+        if (targets.size == 1 && amend) {
+            val oldMessage = parent.lastCommit[world]?.fullMessage ?: return RESULT_CANNOT_AMEND_NO_COMMIT
+            if (message.isEmpty())
+                message = oldMessage
+        }
 
         targets.forEach { IOWorker.addToVersionControl(parent, it) }
 
@@ -67,6 +112,7 @@ class CommitExecutor(parent: Entry): Executor(parent) {
                 git.add().addFilepattern(".").call()
 
                 val commit = git.commit()
+                    .setAmend(amend)
                     .setMessage(message)
                     .setCommitter(PersonIdent("pixel-craft", "pixel.craft@hoonkun.kiwi"))
                     .call()
@@ -88,7 +134,36 @@ class CommitExecutor(parent: Entry): Executor(parent) {
     override fun autoComplete(args: List<String>): MutableList<String> {
         return when (args.size) {
             1 -> parent.repositoryKeys.apply { add("all") }
+            2 -> if (amendMessage(args) != null) SECOND_ARGS_LIST else ARGS_LIST_EMPTY
+            3 -> {
+                if (args[1] != AMEND_ARG) return ARGS_LIST_EMPTY
+
+                val amendMessage = amendMessage(args)
+                if (amendMessage != null) {
+                    mutableListOf(amendMessage)
+                } else {
+                    ARGS_LIST_EMPTY
+                }
+            }
             else -> ARGS_LIST_EMPTY
+        }
+    }
+
+    private fun amendMessage(args: List<String>): String? {
+        val world = args[0]
+        val targets = worldsWithAll(world).toMutableList()
+        targets.removeAll(targets.filter { parent.repositories[it] == null })
+
+        if (targets.size > 1) {
+            val messages = targets.map { parent.lastCommit[it]?.fullMessage }.toSet()
+            if (messages.contains(null))
+                return null
+            if (messages.size != 1)
+                return null
+
+            return messages.toList()[0]
+        } else {
+            return parent.lastCommit[world]?.fullMessage
         }
     }
 
